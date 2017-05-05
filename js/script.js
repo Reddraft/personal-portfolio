3f476e50b3897aea905f870a0518a885c962c8a5
Ricardo <rick@reddraft.com>
2017-05-04 19:29:17 -0500

    Add and style project section. Add full screen modal boxes for each project

--- js/script.js
+++ js/script.js
@@ -9,4 +9,46 @@ $(document).ready(function() {
     $('.menu-overlay').toggleClass('open');
   });
 
+  /*******************************
+  ****     ANIMATED MODAL     ****
+  ********************************/
+
+  // PROJECT 1
+  $("#project-1").animatedModal({
+      modalTarget:'modal-project-1',
+      animatedIn:'zoomInDown',
+      animatedOut:'slideOutDown',
+      animationDuration: '.75s',
+      color: 'transparent',
+      afterClose: function() {
+        $('#modal-project-1').scrollTop(0);
+      }
+
+  });
+
+  // PROJECT 2
+  $("#project-2").animatedModal({
+      modalTarget:'modal-project-2',
+      animatedIn:'zoomInDown',
+      animatedOut:'slideOutDown',
+      animationDuration: '.75s',
+      color: 'transparent',
+      afterClose: function() {
+        $('#modal-project-2').scrollTop(0);
+      }
+
+  });
+
+  // PROJECT 3
+  $("#project-3").animatedModal({
+      modalTarget:'modal-project-3',
+      animatedIn:'zoomInDown',
+      animatedOut:'slideOutDown',
+      animationDuration: '.75s',
+      color: 'transparent',
+      afterClose: function() {
+        $('#modal-project-3').scrollTop(0);
+      }
+  });
+
 });
