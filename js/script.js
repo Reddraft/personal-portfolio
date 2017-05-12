5a73ae24b162f008e818626eaa406334bd5ef301
Ricardo <rick@reddraft.com>
2017-05-12 09:16:34 -0500

    Add stop scrolling class to body when any modal is open

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
     $('.logo').toggleClass('logo-menu-open');
     $('.menu-overlay').toggleClass('open');
     $('html, body').toggleClass('stop-scrolling');
-    $('html, body').css('position','fixed');
   });
 
   $('.main-nav li a').click(function() {
